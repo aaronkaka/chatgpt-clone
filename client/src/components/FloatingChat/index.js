287482cb46812348e2af48acf8c0fee71810c786
Aaron Kaka <aaron.kaka@pearson.com>
2023-03-09 10:58:28 -0500

    Display new Pearson source on new Floating home page.

--- client/src/components/FloatingChat/index.js
+++ client/src/components/FloatingChat/index.js
@@ -11,7 +11,7 @@ const FloatingChat = () => {
       <button title="Contact Sale"
         onClick={() => setOpen((prev) => !prev)}
         className="fixed z-90 bottom-10 right-8 bg-blue-400 w-24 h-24 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-blue-500 hover:drop-shadow-2xl duration-300">
-        <img src={ChatBubbleIcon} className="w-16 h-16" />
+        <img src={ChatBubbleIcon} alt= "chat bubble icon" className="w-16 h-16" />
       </button>
     </div>
   )
--- client/src/pages/FloatingHome.js
+++ client/src/pages/FloatingHome.js
@@ -1,12 +1,13 @@
 import FloatingChat from '../components/FloatingChat';
 import Avatar from '../components/Avatar/Avatar';
 import React from 'react'
-import { motion } from "framer-motion";
+import PearsonSource from '../components/PearsonSource';
+import logo from '../assets/Logo_Horizontal.png'
 
 const FloatingHome = () => {
   return (
     <div className="flex transition duration-500 ease-in-out">
-      <div>THIS IS EXAMPLE CONTENT</div>
+      <div><img src={logo} alt="" width="100" height="100" /> Source Content<br /><br /> {PearsonSource().sourceText}</div>
       <Avatar />
       <FloatingChat />
     </div>
