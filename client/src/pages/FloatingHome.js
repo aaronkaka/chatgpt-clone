7e21c2775aac6fb606be9f70b81e659fb3b549df
Aaron Kaka <aaron.kaka@pearson.com>
2023-03-09 14:32:07 -0500

    Enhance prompt to be more constraining, and break up the source text into paragraphs.

--- client/src/pages/FloatingHome.js
+++ client/src/pages/FloatingHome.js
@@ -7,7 +7,14 @@ import logo from '../assets/Logo_Horizontal.png'
 const FloatingHome = () => {
   return (
     <div className="flex transition duration-500 ease-in-out">
-      <div><img src={logo} alt="" width="100" height="100" /> Source Content<br /><br /> {PearsonSource().sourceText}</div>
+      <div><img src={logo} alt="" width="100" height="100" /> Source Content
+      <br /><br />
+      {PearsonSource().paragraph1}
+      <br /><br />
+      {PearsonSource().paragraph2}
+      <br /><br />
+      {PearsonSource().paragraph3}
+      </div>
       <Avatar />
       <FloatingChat />
     </div>
